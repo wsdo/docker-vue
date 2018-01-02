ed8d8f00fbc7fe30b731b4b7308d5414f1472b23
wangshudong <wsd312@163.com>
2018-01-03 00:07:10 +0800

    init

--- config/api.config.js
+++ config/api.config.js
@@ -1,5 +1,5 @@
 const isProdMode = Object.is(process.env.NODE_ENV, 'production')
 
 module.exports = {
-    baseUrl: isProdMode ? 'http://api.webfenxi.com/v1/' : 'v1/'
+    baseUrl: isProdMode ? 'v1/' : 'v1/'
 }
